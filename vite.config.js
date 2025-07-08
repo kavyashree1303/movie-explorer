69a11605aefd74321f03daaf768ca88dcf0f2eef
Kavyashree H M <kavyashreegowda2003@gmail.com>
2025-07-08 22:53:25 +0530

    changes for deployment

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,7 @@
-// vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/movie-explorer/',  // 👈 this MUST match your repo name
-  plugins: [react()],
-  test: {
-    environment: 'jsdom',
-    globals: true,
-    setupFiles: './src/setupTests.js',
-    css: false
-  }
+  base: '/movie-explorer/', // must match your repo name
+  plugins: [react()]
 });
-
