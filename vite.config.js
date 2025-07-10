3c814be4c190d782c9159ed7c7d9a0ea4949bc87
Kavyashree H M <kavyashreegowda2003@gmail.com>
2025-07-10 09:15:12 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  // ↓ For Netlify the base should be root
-  base: '/',          // or simply delete this line
+  base: '/',         
   plugins: [react()]
 });
